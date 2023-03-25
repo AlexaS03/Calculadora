b1844fffa4e926f18219ac7f4c66aa64c16ec555
Alexandra Sánchez <alexandra.sanchez@mercury-tfs.com>
2023-03-25 12:01:36 -0500

    Se agregaron operaciones matemáticas básicas

--- src/Operaciones/calculadora.java
+++ src/Operaciones/calculadora.java
@@ -1,4 +1,4 @@
-package numeros;
+package Operaciones;
 
 import java.util.Scanner;
 
@@ -7,13 +7,60 @@ public class calculadora {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Scanner tec=new Scanner(System.in);
- 		System.out.println("Seleccionar una opción\n");
- 		System.out.println("+: Suma"
- 				+ "\n-: Resta"
- 				+ "\n*: Multiplicación"
- 				+ "\n/: División");
- 		
+		int a=0, b=0, res=0;
+		String sel=null;
+		try (Scanner tec = new Scanner(System.in)) {
+			System.out.println("Seleccionar una opción\n");
+			System.out.println("+: Suma"
+					+ "\n-: Resta"
+					+ "\n*: Multiplicación"
+					+ "\n/: División");
+			
+			sel=tec.next(); 		
+			switch(sel){
+				case "+":
+				System.out.println("Ha seleccionado Suma: Ingresar Valores\n");
+				System.out.println("Primer Sumando\n");
+				a=tec.nextInt();
+				System.out.println("Segundo Sumando\n");
+				b=tec.nextInt();
+				res=a+b;
+				System.out.println(a+"+"+b+"="+res);
+				break;
+				
+				case "-":
+					System.out.println("Ha seleccionado Resta: Ingresar Valores\n");
+					System.out.println("Minuendoo\n");
+					a=tec.nextInt();
+					System.out.println("Sustraendo\n");
+					b=tec.nextInt();
+					res=a-b;
+					System.out.println(a+"-"+b+"="+res);
+					break;
+				
+				case "*":
+					System.out.println("Ha seleccionado Multiplicación: Ingresar Valores\n");
+					System.out.println("Multiplicando\n");
+					a=tec.nextInt();
+					System.out.println("Multiplicador\n");
+					b=tec.nextInt();
+					res=a*b;
+					System.out.println(a+"*"+b+"="+res);
+					break;	
+					
+				case "/":
+					System.out.println("Ha seleccionado División: Ingresar Valores\n");
+					System.out.println("Divisor\n");
+					a=tec.nextInt();
+					System.out.println("Dividendo\n");
+					b=tec.nextInt();
+					res=a/b;
+					System.out.println(a+"/"+b+"="+res);
+					break;
+					
+			}
+		}
+
 	}
 
 }
